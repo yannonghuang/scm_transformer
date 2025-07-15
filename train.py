6eda7bce8f742b10307cb331ffbb9c989da2f6c7
yannonghuang <yannonghuang@icloud.com>
2025-07-15 16:42:33 +0800

    .

--- train.py
+++
@@ -1,7 +0,0 @@
-from model import train_model, load_model
-from data_utils import load_training_data
-
-def run_training():
-    data = load_training_data("logs/")
-    model = load_model("checkpoint.pt")
-    train_model(model, data)
